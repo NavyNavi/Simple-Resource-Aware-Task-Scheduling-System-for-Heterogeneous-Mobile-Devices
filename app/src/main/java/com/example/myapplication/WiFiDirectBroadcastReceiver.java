badab5a465d6a7803cfec98c1f3417f68454b816
NavyNavi <kcchangrace@gmail.com>
2022-05-15 17:52:29 +0800

    Update WiFi Direct broadcast receiver

--- app/src/main/java/com/example/myapplication/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/example/myapplication/WiFiDirectBroadcastReceiver.java
@@ -5,7 +5,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
 import android.widget.Toast;
 
 @SuppressLint("MissingPermission")
@@ -13,9 +15,11 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     private WifiP2pManager mManager;
     private WifiP2pManager.Channel mChannel;
     private WiFiDirect mActivity;
+    final private String TAG = "WiFiDirectBroadcastReceiver";
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager mManager, WifiP2pManager.Channel mChannel, WiFiDirect mActivity)
     {
+        super();
         this.mManager = mManager;
         this.mChannel = mChannel;
         this.mActivity = mActivity;
@@ -24,6 +28,7 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
+        Log.d(TAG, action);
 
         if(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)){
             int state=intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,-1);
@@ -40,7 +45,6 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                 mManager.requestPeers(mChannel,mActivity.peerListListener);
             }
         }else if(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)){
-            //do something
             if(mManager==null)
             {
                 return;
@@ -50,12 +54,15 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
             if(networkInfo.isConnected())
             {
+                Log.d(TAG, "Connected to p2p network. Requesting network details");
                 mManager.requestConnectionInfo(mChannel,mActivity);
             }else {
                 mActivity.connectionStatus.setText("Device Disconnected");
             }
         }else if(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)){
-            //do something
+            WifiP2pDevice device = (WifiP2pDevice) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_DEVICE);
+            Log.d(TAG, "Device status -" + device.status);
         }
 
     }
